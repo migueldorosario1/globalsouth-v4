2b876a3053035cc3fc700cb259682713942b297d
Refatoracao V4 <refatoracao-v4@local>
2026-08-20 17:25:54 -0300

    fix(blog): correct interviewee name from Ismail Mirandi to Professor Mohammad Marandi

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.globalsouth.news',
-    	integrations: [
+    redirects: {
+        '/blog/brief-202608151436-professor-ismail-mirandi-the-iranians-have-decided-that-enough-is-enou': '/blog/brief-202608151436-professor-mohammad-marandi-the-iranians-have-decided-that-enough-is-enou',
+    },
+    integrations: [
 		mdx(),
 		sitemap({
 			filter: (page) => {
